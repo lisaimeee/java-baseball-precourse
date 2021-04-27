7cdbaefa41d63de7de47b5c139d00ecb704fc112
jess <jess.l@kakaocorp.com>
2021-04-27 22:45:18 +0900

    feat(GameResult): 같은 수가 다른 자리에 있으면 볼로 계산하는 기능 추가

--- src/main/java/GameResult.java
+++ src/main/java/GameResult.java
@@ -2,11 +2,13 @@ public class GameResult {
     private final BaseballGameNumber answer;
     private final BaseballGameNumber guess;
     private int strike;
+    private int ball;
 
     public GameResult(BaseballGameNumber answer, BaseballGameNumber guess) {
         this.answer = answer;
         this.guess = guess;
         countStrike();
+        countBall();
     }
 
     private void countStrike() {
@@ -21,6 +23,18 @@ public class GameResult {
         }
     }
 
+    private void countBall() {
+        if (guess.first() == answer.second() || guess.first() == answer.third()) {
+            ball++;
+        }
+        if (guess.second() == answer.first() || guess.second() == answer.third()) {
+            ball++;
+        }
+        if (guess.third() == answer.first() || guess.third() == answer.second()) {
+            ball++;
+        }
+    }
+
     public boolean isAnswer() {
         return answer.equals(guess);
     }
@@ -31,5 +45,9 @@ public class GameResult {
 
     public int strike() {
         return strike;
+    }
+
+    public int ball() {
+        return ball;
     }
 }
--- src/test/java/GameResultTest.java
+++ src/test/java/GameResultTest.java
@@ -41,11 +41,40 @@ public class GameResultTest {
     }
     static Stream<Arguments> provider_같은_수가_같은_자리에_있으면_스트라이크이다(){
         BaseballGameNumber answer = new BaseballGameNumber(1,2,3);
+        BaseballGameNumber 일치하지_않음 = new BaseballGameNumber(4,5,6);
+        BaseballGameNumber 같은수_같은자리_하나일치 = new BaseballGameNumber(1,5,6);
+        BaseballGameNumber 같은수_같은자리_두개일치 = new BaseballGameNumber(1,2,6);
+        BaseballGameNumber 같은수_같은자리_세개일치 = new BaseballGameNumber(1,2,3);
+
         return Stream.of(
-                Arguments.of(answer, new BaseballGameNumber(4,5,6), 0),
-                Arguments.of(answer, new BaseballGameNumber(1,5,6), 1),
-                Arguments.of(answer, new BaseballGameNumber(1,2,6), 2),
-                Arguments.of(answer, new BaseballGameNumber(1,2,3), 3)
+                Arguments.of(answer, 일치하지_않음, 0),
+                Arguments.of(answer, 같은수_같은자리_하나일치, 1),
+                Arguments.of(answer, 같은수_같은자리_두개일치, 2),
+                Arguments.of(answer, 같은수_같은자리_세개일치, 3)
+        );
+    }
+
+    @DisplayName("같은_수가_다른_자리에_있으면_볼이다")
+    @ParameterizedTest
+    @MethodSource("provider_같은_수가_다른_자리에_있으면_볼이다")
+    void 같은_수가_다른_자리에_있으면_볼이다(BaseballGameNumber answer, BaseballGameNumber guess, int expectedBall){
+        GameResult result = new GameResult(answer, guess);
+        assertThat(result.ball())
+                .isEqualTo(expectedBall);
+    }
+    static Stream<Arguments> provider_같은_수가_다른_자리에_있으면_볼이다(){
+        BaseballGameNumber answer = new BaseballGameNumber(1,2,3);
+        BaseballGameNumber 일치하는_수_없음 = new BaseballGameNumber(4,5,6);
+        BaseballGameNumber 같은수_모두_같은자리 = new BaseballGameNumber(1,2,3);
+        BaseballGameNumber 다른자리_하나_일치 = new BaseballGameNumber(4,1,6);
+        BaseballGameNumber 다른자리_둘_일치 = new BaseballGameNumber(2,3,6);
+        BaseballGameNumber 다른자리_셋_일치 = new BaseballGameNumber(3,1,2);
+        return Stream.of(
+                Arguments.of(answer, 일치하는_수_없음, 0),
+                Arguments.of(answer, 같은수_모두_같은자리, 0),
+                Arguments.of(answer, 다른자리_하나_일치, 1),
+                Arguments.of(answer, 다른자리_둘_일치, 2),
+                Arguments.of(answer, 다른자리_셋_일치, 3)
         );
     }
 }
